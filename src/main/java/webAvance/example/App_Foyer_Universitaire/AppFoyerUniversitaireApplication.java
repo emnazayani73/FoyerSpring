f197747873e0c55f8c59e58e9c0924fcf1c49534
emnazayani73 <92338155+emnazayani73@users.noreply.github.com>
2024-12-24 13:20:24 +0100

    Premier commit : ajout du projet Spring Boot

---
+++ src/main/java/webAvance/example/App_Foyer_Universitaire/AppFoyerUniversitaireApplication.java
@@ -0,0 +1,13 @@
+package webAvance.example.App_Foyer_Universitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFoyerUniversitaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFoyerUniversitaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webAvance/example/App_Foyer_Universitaire/AppFoyerUniversitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package webAvance.example.App_Foyer_Universitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFoyerUniversitaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
