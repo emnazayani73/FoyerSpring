f888749c45497d30c3c7c66789d4b7cacee6e9a6
safeselmi1 <s.safeselmi@gmail.com>
2024-12-29 22:51:44 +0100

    gestion etudiant

---
+++ src/main/java/webAvance/example/App_Foyer_Universitaire/controller/EtudiantController.java
@@ -0,0 +1,4 @@
+package webAvance.example.App_Foyer_Universitaire.controller;
+
+public class EtudiantController {
+}
---
+++ src/main/java/webAvance/example/App_Foyer_Universitaire/service/EtudiantService.java
@@ -0,0 +1,4 @@
+package webAvance.example.App_Foyer_Universitaire.service;
+
+public class EtudiantService {
+}
